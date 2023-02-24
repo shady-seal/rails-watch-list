d8b446aea1879a638fcb8e1184abff71108d6799
Sofia Eriksson <shadyseal@zohomail.eu>
2023-02-24 11:16:46 +0000

    add dotenv-rails

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,9 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  resources :movies, only: [:index, :show]
+  resources :lists, only: [:index, :show, :new, :create, :destroy] do
+    resources :bookmarks, only: [:new, :create]
+  end
+  resources :bookmarks, only: [:destroy]
 end
